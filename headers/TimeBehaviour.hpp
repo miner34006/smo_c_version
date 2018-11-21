1cc22ba0e8250e757568445edfb6ee0cfa0b2b1e
Polianok Bogdan <miner34006@gmail.com>
2018-11-21 08:59:52 +0300

    Implement some structure of time behavior

--- headers/TimeBehaviour.hpp
+++ headers/TimeBehaviour.hpp
@@ -5,9 +5,30 @@
 #ifndef SMO_TIMEBEHAVIOUR_HPP
 #define SMO_TIMEBEHAVIOUR_HPP
 
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+
 class TimeBehaviour {
 public:
   virtual double generateTime(const double &intensity) = 0;
+};
+
+class ExponentialTimeBehavior: public TimeBehaviour {
+public:
+  double generateTime(const double &intensity) override;
+};
+
+
+class InfiniteUniformTimeBehavior: public TimeBehaviour {
+public:
+  InfiniteUniformTimeBehavior(const double& a, const double& b);
+  double generateTime(const double &intensity) override;
+
+private:
+  double a_;
+  double b_;
 };
 
 #endif //SMO_TIMEBEHAVIOUR_HPP
--- src/TimeBehaviour.cpp
+++ src/TimeBehaviour.cpp
@@ -2,4 +2,28 @@
 // Created by Богдан Полянок on 21.09.2018.
 //
 
+#include <stdexcept>
 #include "../headers/TimeBehaviour.hpp"
+
+double ExponentialTimeBehavior::generateTime(const double &intensity) {
+  // TODO: add time generation strategy
+  if (intensity < 0) {
+    throw std::invalid_argument("Intensity should be > 0");
+  }
+  const double randomValue = ((double) std::rand() / (RAND_MAX));
+  return std::log(1  + std::exp(-intensity * randomValue)) / -intensity;
+}
+
+double InfiniteUniformTimeBehavior::generateTime(const double &intensity) {
+  // TODO: add time generation strategy
+  if (intensity < 0) {
+    throw std::invalid_argument("Intensity should be > 0");
+  }
+  const double randomValue = ((double) std::rand() / (RAND_MAX));
+  return 0;
+}
+
+InfiniteUniformTimeBehavior::InfiniteUniformTimeBehavior(const double &a, const double &b):
+  a_(a),
+  b_(b)
+{}
